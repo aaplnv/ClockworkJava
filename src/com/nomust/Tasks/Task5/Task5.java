c6576cf60b64806ec92e2655a1f710e18066052f
mr-poluyanov <56654805+aaplnv@users.noreply.github.com>
2021-10-12 14:09:55 +0300

    NEW: Task5

--- src/com/nomust/Tasks/Task5/Task5.java
+++ src/com/nomust/Tasks/Task5/Task5.java
@@ -12,9 +12,21 @@ public class Task5 extends Task {
 
     @Override
     public void Run() {
+        System.out.println("GetCount: ");
         System.out.println("Enter sequence (0 to finish)");
-            getCount(0, 0);
-            pressAnyKey();
+
+        getCount(0, 0);
+        System.out.println("\n\nGetMaximum: ");
+        System.out.println("Enter sequence (0 to finish)");
+        System.out.println(getMaximum());
+        System.out.println("\n\nGetReverse: ");
+        System.out.println("Enter sequence (0 to finish)");
+
+        Scanner keyboard = new Scanner(System.in);
+        int n = keyboard.nextInt(); // TODO: Keyboard reader with error handler (how to code it without most of console functions?)
+        System.out.println(getReverse(n, 0));
+
+        pressAnyKey();
     }
 
     static void getCount(int max, int count)
@@ -23,6 +35,7 @@ public class Task5 extends Task {
         int n = keyboard.nextInt(); // TODO: Keyboard reader with error handler (how to code it without most of console functions?)
         if (n == 0) {
             System.out.println(max + ", " + count);
+            return;
         }
         if (n > max) {
             getCount(n, 1);
@@ -31,5 +44,20 @@ public class Task5 extends Task {
         } else {
             getCount(max, count);
         }
+    }
+
+    public static int getMaximum() {
+        Scanner keyboard = new Scanner(System.in);
+        int n = keyboard.nextInt(); // TODO: Keyboard reader with error handler (how to code it without most of console functions?)
+        if (n == 0) {
+            return 0;
+        }
+        else {
+            return Math.max(n, getMaximum());
+        }
+    }
+
+    public static int getReverse(int n, int i) {
+        return (n==0) ? i : getReverse( n/10, i*10 + n%10 );
     }
 }
